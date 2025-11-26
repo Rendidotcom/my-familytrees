46bd635ba563ab3082a270b19de62e6c2c813f61
Rendidotcom <holis112@gmail.com>
2025-11-26 14:16:14 +0700

    config all

---
+++ config.js
@@ -0,0 +1,4 @@
+// ==============================
+// 🌳 Family Tree Global Config
+// ==============================
+export const API_URL = "https://script.google.com/macros/s/AKfycbz2PlRiv2hJtvmbZJtgYkMm9Cf2DjGYTdVkCz0LG-r2bi7SVB8pCvHR_YOIiw0A4bnCPA/exec";
